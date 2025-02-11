b04b0e2329e517ba010482831c0817be1012f99c
satriarangga17 <msatriarangga08@gmail.com>
2025-02-11 07:04:20 +0700

    Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
       <nav className="bg-black p-6 text-white h-20 fixed top-0 right-0 left-0">
       <div className="container mx-auto flex justify-between">
-        <h1 className="text-2xl font-bold">PORTOFOLIO</h1>
+        <h1 className="text-2xl font-bold">PORTFOLIO</h1>
         <ul className="flex space-x-4">
           <li><a href="/" className="hover:text-gray-300">Home</a></li>
           <li><a href="/projects" className="hover:text-gray-300">Projects</a></li>
